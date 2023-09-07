f4be8a249052aa1858f0102706fdde50eb92ad7b
agamenonteccent <teccent2022@gmail.com>
2023-09-07 19:23:28 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div id="app">
+    <NomeENotas @adicionar="adicionarNomeENotas" />
+    <ListaNomesENotas :itens="nomesENotas" />
+  </div>
+</template>
+
+<script>
+
+import NomeENotas from "./components/NomeENotas.vue";
+import ListaNomesENotas from "./components/ListaNomesENotas.vue";
+
+export default {
+  components: {
+    NomeENotas,
+    ListaNomesENotas,
+  },
+  data() {
+    return {
+      nomesENotas: [],
+    };
+  },
+  methods: {
+    adicionarNomeENotas(dados) {
+      this.nomesENotas.push(dados);
+    },
+  },
+};
+</script>
+<style>
+
+*{
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
